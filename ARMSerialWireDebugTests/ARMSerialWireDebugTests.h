f6296918ec597d4ca65b1dc10a97f77fe799b692
Denis Bohm <denis@fireflydesign.com>
2014-03-18 20:38:31 -0500

    update project

--- ARMSerialWireDebugTests/ARMSerialWireDebugTests.h
+++ ARMSerialWireDebugTests/ARMSerialWireDebugTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Firefly Design. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface ARMSerialWireDebugTests : SenTestCase
+@interface ARMSerialWireDebugTests : XCTestCase
 
 @end
